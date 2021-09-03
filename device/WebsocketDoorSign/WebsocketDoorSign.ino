8a938115935749e66e8ba505108d1d0c706dc442
Shane Fleming <shane.t.fleming@gmail.com>
2021-09-03 22:32:41 +0100

    Basic websocket link, with the server on the device, is working.

--- device/WebsocketDoorSign/WebsocketDoorSign.ino
+++ device/WebsocketDoorSign/WebsocketDoorSign.ino
@@ -1,1 +1,49 @@
+// Adapted from the ArduinoWebsockets server example
+#include <ArduinoWebsockets.h>
+#include <WiFi.h>
 
+const char* ssid = "stf_0x2a";
+const char* password = "jyrs3CkNqtqy";
+
+using namespace websockets;
+
+WebsocketsServer server;
+void setup() {
+  Serial.begin(115200);
+  // Connect to wifi
+  WiFi.begin(ssid, password);
+
+  // Wait some time to connect to wifi
+  for(int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) {
+      Serial.print(".");
+      delay(1000);
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());   //You can get IP address assigned to ESP
+
+  server.listen(4242);
+  Serial.print("Is server live? ");
+  Serial.println(server.available());
+}
+
+void loop() {
+  WebsocketsClient client = server.accept();
+  if(client.available()) {
+    WebsocketsMessage msg = client.readBlocking();
+
+    // log
+    Serial.print("Got Message: ");
+    Serial.println(msg.data());
+
+    // return echo
+    client.send("Echo: " + msg.data());
+
+    // close the connection
+    client.close();
+  }
+  
+  delay(1000);
+}
